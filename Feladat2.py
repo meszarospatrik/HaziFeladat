72e7bf7b8c06f638c823250e893cac386df7dff4
meszarospatrik <36508270+meszarospatrik@users.noreply.github.com>
2018-03-08 17:28:28 +0100

    Update Feladat2.py

--- Feladat2.py
+++ Feladat2.py
@@ -9,6 +9,26 @@ def feladat_9():
         if k==300:
             return print(x)
 
+        
+        
+        
+        
+def feladat_10():
+    try:
+        fajl=open(fajl_nev,mode="r")
+        betuk=string.split(fajl)
+        betukszama=0
+        for i in range(len(fajl)):
+            if fajl.isupper():
+                for i in betuk:
+                    betukszama += len(i)
+        return betukszama
+    except OSError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        fajl.close()
 
 
 
