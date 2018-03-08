ba8bb6d40814fd6ef303a643c342af0d2de2e946
meszarospatrik <36508270+meszarospatrik@users.noreply.github.com>
2018-03-08 17:07:33 +0100

    Add files via upload

---
+++ Feladat2.py
@@ -0,0 +1,49 @@
+
+def feladat_9():
+    n=1
+    k=1
+    x=0
+    for i in range():
+        k+(1/n+1)
+        x+=1
+        if k==300:
+            return print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
