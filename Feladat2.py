d1ebcb03290daeaa64870011a8d289e529fc68a5
meszarospatrik <36508270+meszarospatrik@users.noreply.github.com>
2018-03-12 23:24:12 +0100

    Update Feladat2.py

--- Feladat2.py
+++ Feladat2.py
@@ -31,7 +31,42 @@ def feladat_10():
         fajl.close()
 
 
+def osztok_szama(szam):
+    db=2
+    for i in range(2,szam//2+1):
+        if szam%i==0:
+            db=db+1
 
+    return db
+
+
+
+def feladat_5(n):
+    max=1
+    for i in range(2,n+1):
+        if max<osztok_szama(i):
+            max=osztok_szama(i)
+            print(i)
+            
+            
+            
+   def feladat_15():
+    try:
+        fajl1=open("be.txt",mode="r")
+        fajl2=open("ki.txt",mode="w")
+        sor=fajl1.readline()
+        if bool(sor)==False:
+            sor=sor.strip()
+        else:
+            return sor
+        fajl2.write("%d","%d" % (sor))
+        fajl1.close()
+        fajl2.close()
+
+    except Exception as e:
+        print(e)         
+            
+    
 
 
 
