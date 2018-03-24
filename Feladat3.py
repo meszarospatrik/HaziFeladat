3d2928bb9e1b1c1d6fa2be91a700f076f48cd4f0
meszarospatrik <36508270+meszarospatrik@users.noreply.github.com>
2018-03-24 14:55:28 +0100

    Add files via upload

---
+++ Feladat3.py
@@ -0,0 +1,38 @@
+def feladat_3(n):
+    tomb=[]
+    for i in range(n):
+        x=int(input())
+        tomb.append(x)
+
+
+    return sum(tomb)/n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    print(feladat_3(2))
+
+
+if __name__ == "__main__":
+    main()
+
